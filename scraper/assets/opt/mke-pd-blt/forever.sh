65dcd41cb6aecb5d243bf60a32ff1fedcf248cf8
Matt Boelter <boelter@gmail.com>
2017-07-26 02:05:00 -0500

    Add schema initialization to scraper

--- scraper/assets/opt/mke-pd-blt/forever.sh
+++ scraper/assets/opt/mke-pd-blt/forever.sh
@@ -2,6 +2,22 @@
 
 SLEEP=120
 
+database_ready() {
+    curl --silent --connect-timeout 1 http://${PGHOST}:${PGPORT}
+    echo $?
+}
+
+echo "Waiting for database to be ready..."
+while [ $(database_ready) -ne  "52" ]; do
+    sleep 1
+done
+
+set -e
+
+echo "Run schema initialization..."
+node init-schema.js
+
+echo "Begin polling data source..."
 while [ true ]; do
     node scraper.js
     sleep ${SLEEP}
---
+++ scraper/assets/opt/mke-pd-blt/init-schema.js
@@ -0,0 +1,33 @@
+const pg = require('pg');
+const client = new pg.Client();
+client.connect();
+
+client.query(`
+    CREATE TABLE IF NOT EXISTS calls
+    (
+      number    BIGINT PRIMARY KEY,
+      date_time TIMESTAMP DEFAULT NOW(),
+      location  TEXT      DEFAULT 'Unknown',
+      district  SMALLINT  DEFAULT -1,
+      nature    TEXT      DEFAULT 'Unknown',
+      status    TEXT      DEFAULT 'Unknown'
+    );
+`, (err, res) => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    client.query(`
+        CREATE INDEX calls_idx_date_time ON calls (date_time);
+        CREATE INDEX calls_idx_location ON calls (location);
+        CREATE INDEX calls_idx_district ON calls (district);
+        CREATE INDEX calls_idx_nature ON calls (nature);
+        CREATE INDEX calls_idx_status ON calls (status);
+    `, (err, res) => {
+        console.log('Schema initialized...');
+        client.end();
+        process.exit(0);
+    });
+
+});
