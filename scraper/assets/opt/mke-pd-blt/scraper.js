31aae3f79fb99fe5b9cf73c8f930a9e7422b2e2d
Matt Boelter <boelter@gmail.com>
2017-07-26 02:11:24 -0500

    Refactor scraper with slightly more modern style

--- scraper/assets/opt/mke-pd-blt/scraper.js
+++ scraper/assets/opt/mke-pd-blt/scraper.js
@@ -1,21 +1,21 @@
-var http = require('http');
-var htmlparser = require('htmlparser');
-var util = require('util');
-var select = require('soupselect-update').select;
-var moment = require('moment');
+const http = require('http');
+const htmlparser = require('htmlparser');
+const util = require('util');
+const select = require('soupselect-update').select;
+const moment = require('moment');
 
-var options = {
+const options = {
     host: 'itmdapps.milwaukee.gov',
     path: '/MPDCallData/currentCADCalls/callsService.faces'
 };
 
-var parser = new htmlparser.Parser(new htmlparser.DefaultHandler(function(err, dom) {
+const parser = new htmlparser.Parser(new htmlparser.DefaultHandler((err, dom) => {
     if (err) {
         throw err;
     }
 
     // last update
-    var update = select(dom, '#formId:updatedId');
+    let update = select(dom, '#formId:updatedId');
     if (update.length !== 1 || update[0].children.length !== 1) {
         throw 'Unexpected number of elements for update.';
     }
@@ -27,7 +27,7 @@ var parser = new htmlparser.Parser(new htmlparser.DefaultHandler(function(err, d
     ///////////
 
     // number of calls
-    var total = select(dom, '#formId:textTotalCallId');
+    let total = select(dom, '#formId:textTotalCallId');
     if (total.length !== 1 || total[0].children.length !== 1) {
         throw 'Unexpected number of elements for total.';
     }
@@ -42,15 +42,11 @@ var parser = new htmlparser.Parser(new htmlparser.DefaultHandler(function(err, d
     }
 
     // assume 6 col x 0-20 rows
-    var todo = total;
-
-    console.log(todo);
-
-    var rowToCall = function (index) {
-        var call = {};
-        var call_number = select(dom, '#formId:tableExUpdateId:' + index + ':text1');
-        if (call_number.length === 1 && call_number[0].children.length === 1) {
-            call.call_number = call_number[0].children[0].data;
+    let rowToCall = function (index) {
+        let call = {};
+        let number = select(dom, '#formId:tableExUpdateId:' + index + ':text1');
+        if (number.length === 1 && number[0].children.length === 1) {
+            call.number = number[0].children[0].data;
             call.date_time = moment(select(dom, '#formId:tableExUpdateId:' + index + ':text2')[0].children[0].data, 'MM/DD/YYYY hh:mm:ss A');
             call.location = select(dom, '#formId:tableExUpdateId:' + index + ':text3')[0].children[0].data;
             call.district = select(dom, '#formId:tableExUpdateId:' + index + ':text4')[0].children[0].data;
@@ -60,28 +56,25 @@ var parser = new htmlparser.Parser(new htmlparser.DefaultHandler(function(err, d
         return call;
     };
 
-    var calls = [];
-    for (var i = 0; i < 20; i++) {
-        var call = rowToCall(i);
-        if (call.call_number) {
+    let calls = [];
+    for (let i = 0; i < 20; i++) {
+        let call = rowToCall(i);
+        if (call.number) {
             calls.push(call);
         }
     }
 
-
     console.log(util.inspect(calls));
 }));
 
-callback = function(response) {
-    var str = '';
+http.request(options, (response) => {
+    let str = '';
 
-    response.on('data', function (chunk) {
+    response.on('data', (chunk) => {
         str += chunk;
     });
 
-    response.on('end', function () {
+    response.on('end', () => {
         parser.parseComplete(str);
     });
-};
-
-http.request(options, callback).end();
+}).end();
