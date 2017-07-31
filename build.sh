abd021c66da9c5a60b793e7d20368ffdec2c2f17
Matt Boelter <boelter@gmail.com>
2017-07-31 21:13:25 +0000

    Fix build order for static site

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ build_static_site() {
     pushd site/assets/opt/mke-pd-blt
     bower install
     popd
+    docker build -t mke-pd-blt-site site
 }
 
 build_static_site &
 docker pull postgres:9.6.3 &
 docker build -t mke-pd-blt-scraper scraper &
 docker build -t mke-pd-blt-server server &
-docker build -t mke-pd-blt-site site &
 
 wait
